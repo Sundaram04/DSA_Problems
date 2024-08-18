50df99e94382358b3d355e37e38394fbf407205e
Sundaram04 <145194021+Sundaram04@users.noreply.github.com>
2024-08-18 23:39:48 +0530

    Right Triangle Pattern
    Right triangle pattern with stars (*) where the number of rows is specified by the user.

---
+++ Pattern_Problems/Pattern_2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sq_pattern(int x) {
+    for(int i = 0; i < x; i++) {
+        for(int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    sq_pattern(n);
+}
