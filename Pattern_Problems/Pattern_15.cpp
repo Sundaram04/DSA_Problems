95d6bc881033d96e92cb51914bb93853cb2f80da
Sundaram04 <145194021+Sundaram04@users.noreply.github.com>
2024-09-09 19:33:42 +0530

    Inverse Right triangle of letters

---
+++ Pattern_Problems/Pattern_15.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void pattern(int n){
+    for(int i=0; i<n; i++) {
+        for(char ch = 'A'; ch < 'A'+n-i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+    int x;
+    cin >> x;
+    pattern(x);
+}
